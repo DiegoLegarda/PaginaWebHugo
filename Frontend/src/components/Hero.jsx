ddce70f71067f30c388c22c3dc61e70833459684
diegolegarda <legarda8027@gmail.com>
2025-06-23 22:18:26 -0500

    ajusta del banner

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -4,19 +4,15 @@ function Hero() {
   return (
     <motion.section
       id="hero"
-      className="relative w-screen h-screen bg-cover bg-center text-white overflow-hidden"
+      className="relative w-screen min-h-screen bg-cover bg-center text-white flex items-center justify-center px-4"
       style={{ backgroundImage: 'url(/2.png)' }}
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
     >
-
-
-      
-      
-
+     
     </motion.section>
-  )
+  );
 }
 
 export default Hero;
