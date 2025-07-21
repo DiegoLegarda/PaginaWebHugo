69b0f59d411ab72d064082e143f844d59eae87df
diegolegarda <legarda8027@gmail.com>
2025-07-21 08:21:54 -0500

    cambio d einformacion de carrera

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
     >
       <h2 className="text-3xl font-bold mb-6 text-center">Sobre el Evento</h2>
       <p className="text-lg text-center max-w-3xl mx-auto">
-       Running Popayaán es un evento que promueve la sana competencia y el amor por el deporte en la hermosa ciudad de Popayán. Con distancias de 5K, y 10K, este evento está diseñado para corredores de todos los niveles. Únete a nosotros el 16 de noviembre de 2025 para una experiencia inolvidable llena de energía, camaradería y pasión por el running.
+       Running Popayán es un evento que promueve la sana competencia y el amor por el deporte en la hermosa ciudad de Popayán. Con distancias de 6K, y 12K, este evento está diseñado para corredores de todos los niveles. Únete a nosotros en noviembre de 2025 para una experiencia inolvidable llena de energía, camaradería y pasión por el running.
       </p>
     </motion.section>
   )
--- Frontend/src/components/Categories.jsx
+++ Frontend/src/components/Categories.jsx
@@ -3,8 +3,8 @@ function Categories() {
     <section id="categories" style={{ padding: '2rem' }}>
       <h2>Categorías</h2>
       <ul>
-        <li>5K Recreativa</li>
-        <li>10K Competitiva</li>
+        <li>6K Recreativa</li>
+        <li>12K Competitiva</li>
         </ul>
     </section>
   )
