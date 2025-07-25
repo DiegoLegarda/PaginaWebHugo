3fd338b41be5ffb5b470ab9bdff4cb5d74f2ae06
diegolegarda <legarda8027@gmail.com>
2025-07-24 19:13:30 -0500

    actualizacion de Hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       {/* Contenedor de imagen - Versión mejorada */}
       <div className="absolute inset-0 flex items-center justify-center">
         <img
-          src="/RUNNING2.svg"
+          src="/test9.png"
           alt="Fondo hero"
           className="min-w-full min-h-full object-cover"
           style={{
@@ -24,8 +24,8 @@ const Hero = () => {
       {/* Contenido */}
       <div className="relative z-10 h-full flex items-center justify-center text-center px-4 md:px-8 animate-fadeIn font-sans">
         <div className="text-white max-w-2xl">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">NOVIEMBRE</h1>
-          <p className="text-lg md:text-2xl mb-6">
+          <h1 className="text-5xl md:text-9xl font-bold mb-4">NOVIEMBRE</h1>
+          <p className="text-lg md:text-6xl mb-6">
             desde el Cauca corremos por la vida
           </p>
         </div>
