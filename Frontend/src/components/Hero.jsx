840902a1704c4a5927bd342e18ab47a271eba8ac
diegolegarda <legarda8027@gmail.com>
2025-08-02 09:49:59 -0500

    cambio de Hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       {/* Contenedor de imagen - Versión mejorada */}
       <div className="absolute inset-0 flex items-center justify-center">
   <img
-    src="/test9.png"
+    src="/test5.jpg"
     alt="Fondo hero"
     // Eliminamos las clases min-w-full y min-h-full que la estiraban
     style={{
