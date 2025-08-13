5e6aa539e9d031c7473d5dd14a57dfaa083a6d23
DiegoLegarda <legarda8027@gmail.com>
2025-08-13 11:17:16 -0500

    cambio en footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer style={{ background: '#333', color: '#fff', padding: '1rem', textAlign: 'center' }}>
-      <p>&copy; 2025 Media Maratón de Popayán. Todos los derechos reservados.</p>
+      <p>&copy; 2025 Running Popayán. Todos los derechos reservados.</p>
       <p>
         <Link to="/terminos" className="text-blue-300 hover:text-white underline">
           Términos y Condiciones
