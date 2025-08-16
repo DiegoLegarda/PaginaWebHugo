d30596688da1fd765eb4bc7200cef15c9fac77f5
DiegoLegarda <legarda8027@gmail.com>
2025-08-16 02:28:19 -0500

    actualizacion Whatsapp

--- Frontend/src/components/SocialMediaFloating.jsx
+++ Frontend/src/components/SocialMediaFloating.jsx
@@ -13,8 +13,22 @@ function SocialMediaFloating() {
     ? 'instagram://user?username=runningpopayan1'
     : 'https://www.instagram.com/runningpopayan1';
 
+  const whatsappLink = isMobile
+    ? 'whatsapp://send?phone=573014554318&text=Hola%20quiero%20m%C3%A1s%20informaci%C3%B3n'
+    : 'https://wa.me/573014554318?text=Hola%20quiero%20m%C3%A1s%20informaci%C3%B3n';
+
   return (
     <div className="fixed left-4 bottom-4 flex flex-col gap-4 z-50">
+      {/* WhatsApp */}
+      <a
+        href={whatsappLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-white text-blue-600 w-10 h-10 rounded-full shadow-lg hover:bg-gray-100 flex items-center justify-center"
+      >
+        <img src="/logoW.png" alt="WhatsApp" className="w-10 h-10" />
+      </a>
+
       {/* Facebook */}
       <a
         href={facebookLink}
