e86a2b6782b5d13a95efb3cbcab03c080f7c7c4c
DiegoLegarda <legarda8027@gmail.com>
2025-10-03 21:04:36 -0500

    se quitan categorias

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
         <Hero/>     
         <Countdown />
         <About />        
-        <Categories />      
+              
         <RouteMap />
         <SponsorsCarousel />
         <Contact />
